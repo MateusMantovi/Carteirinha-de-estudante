cdd81d6765a314c6f61ecca8543cba3fc173b18b
Mateus Mantovi <mateusmantovi@gmail.com>
2025-03-24 20:26:14 -0300

    2,0 Segunda fase botão modo escuro

--- scripts.js
+++ scripts.js
@@ -1,12 +1,39 @@
 const meuPrimeiroApp = Vue.createApp({
   data() {
     return {
+      // Dados originais mantidos
       mensagemInicial: "Bem-vindo ao Vue.js",
       msgInicial: "Esta é uma mensagem inicial",
       verMsg: false,
       corMsg: "",
       cores: [{ ptbr: "AZUL" }, { ptbr: "VERDE" }, { ptbr: "VERMELHO" }],
+
+      // Novos dados para a funcionalidade 2.0
+      novoItem: "",
+      itens: [],
+      darkMode: false,
     };
+  },
+  methods: {
+    adicionarItem() {
+      if (this.novoItem.trim() !== "") {
+        this.itens.push(this.novoItem);
+        this.novoItem = "";
+      }
+    },
+    removerItem(index) {
+      this.itens.splice(index, 1);
+    },
+  },
+  watch: {
+    darkMode(newVal) {
+      // Garante que a classe seja aplicada ao body também se necessário
+      if (newVal) {
+        document.body.classList.add("dark-theme");
+      } else {
+        document.body.classList.remove("dark-theme");
+      }
+    },
   },
 });
 
